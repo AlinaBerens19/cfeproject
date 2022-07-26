322368774643bb07e108b0edd9f65b211027bd58
alina <berens.alina019@gmail.com>
2022-07-26 14:46:28 +0300

    Added backend/

---
+++ backend/api/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from products.models import Product
+from django.forms.models import model_to_dict
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from products.serializers import ProductSerializer
+import json
+
+@api_view(['POST'])
+def api_home(request, *args, **kwargs):
+
+    serializer = ProductSerializer(data=request.data)
+    if serializer.is_valid(raise_exception=True):
+        # instance = serializer.save()
+        # instance = form.save()
+        print(serializer.data)
+        return Response(serializer.data)
+    return Response({"invalid": "not good data"}, status=400)
